f4b1c4cf9b9ded54b27cd1a73ff736fef3950af4
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-10-13 22:21:49 +0530

    1

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-
-//security packages
+import path from "path";
+//securty packges
 import helmet from "helmet";
 import dbConnection from "./dbConfig/index.js";
-import errorMiddleWare from "./middleware/errorMddleWear.js";
+import errorMiddleware from "./middleware/errorMddleWear.js";
 import router from "./routes/index.js";
+
+const __dirname = path.resolve(path.dirname(""));
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.port || 8800;
+app.use(express.static(path.join(__dirname, "views/build")));
+
+const PORT = process.env.PORT || 8800;
 
 dbConnection();
 
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.json({limit: "10mb"}));
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
 app.use(router);
-//error middleware
-app.use(errorMiddleWare)
 
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+//error middleware
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
