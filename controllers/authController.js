8a5797cc1ba4837a5505d145b496d158ebb42e0a
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-10-15 23:35:23 +0530

    1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,90 +1,89 @@
 import Users from "../models/userModel.js";
-import { comparePassword, createJWT, hashString } from "../utils/index.js";
+import { compareString, createJWT, hashString } from "../utils/index.js";
+import { sendVerificationEmail } from "../utils/sendEmail.js";
 
 export const register = async (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
-    //VALIDATE FIELDS
-    if (!(firstName || lastName || email || password)) {
-        next("Provide required fields!");
-        return;
+  //validate fileds
+//   if (!(firstName || lastName || email || password)) {
+//     next("Provide Required Fields!");
+//     return;
+//   }
+
+  try {
+    const userExist = await Users.findOne({ email });
+
+    if (userExist) {
+      next("Email Address already exists");
+      return;
     }
 
-    try {
-        //CHEKCING WEATHER EMAIL ID ALREADY REGSITERED OR NOT
-        const userExist = await Users.findOne({ email });
+    const hashedPassword = await hashString(password);
 
-        if (userExist) {
-            next("Email address already exists");
-            return
-        }
+    const user = await Users.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
 
-        const hashedPassword = await hashString(password);
-
-        const user = await Users.create({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-        });
-
-        //SEND VERIFICATION EMAIL TO USER
-        sendVerificationEmail(user, res);
-
-    }
-    catch (error) {
-        console.log(error);
-        res.status(404).json({ message: error.message })
-    }
-}
-
+    //send email verification to user
+    sendVerificationEmail(user, res);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const login = async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        //VALIDATION
-        if (!(email || password)) {
-            next("Provide required fields!");
-            return;
-        }
-        //FIND USER BY EMAIL 
-        const user = await Users.findOne({ email }).select("+password").populate({
-            path: "friends",
-            select: "firstName lastName email location profileUrl password",
-        });
-
-        //USER EXIST OR NOT
-
-        if (!user) {
-            next("Invalid email or password");
-            return;
-        }
-
-        if (!user?.verified) {
-            next("Please verify your email address");
-            return;
-        }
-
-        const isMatch = await comparePassword(password, user?.password)
-
-        if(!isMatch){
-            next("Invalid email or password");
-            return;
-        }
-
-        user.password = undefined;
-        const token = createJWT(user?._id);
-
-        res.status(201).json({
-            success:true,
-            message:"Login successfully",
-            user,
-            token,
-        })
-
+  try {
+    //validation
+    if (!email || !password) {
+      next("Please Provide User Credentials");
+      return;
     }
-    catch (error) {
-        console.log(error);
+
+    // find user by email
+    const user = await Users.findOne({ email }).select("+password").populate({
+      path: "friends",
+      select: "firstName lastName location profileUrl -password",
+    });
+
+    if (!user) {
+      next("Invalid email or password");
+      return;
     }
-}
+
+    if (!user?.verified) {
+      next(
+        "User email is not verified. Check your email account and verify your email"
+      );
+      return;
+    }
+
+    // compare password
+    const isMatch = await compareString(password, user?.password);
+
+    if (!isMatch) {
+      next("Invalid email or password");
+      return;
+    }
+
+    user.password = undefined;
+
+    const token = createJWT(user?._id);
+
+    res.status(201).json({
+      success: true,
+      message: "Login successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
--- utils/index.js
+++ utils/index.js
@@ -1,22 +1,21 @@
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
-export const hashString =  async (userValue) => {
-    const salt = await bcrypt.genSalt(10);
+export const hashString = async (useValue) => {
+  const salt = await bcrypt.genSalt(10);
 
-    const hashedPassword = await bcrypt.hash(password, salt);
-    return hashedPassword;
+  const hashedpassword = await bcrypt.hash(useValue, salt);
+  return hashedpassword;
+};
 
-}
+export const compareString = async (userPassword, password) => {
+  const isMatch = await bcrypt.compare(userPassword, password);
+  return isMatch;
+};
 
-export const comparePassword = async (userPassword,password) =>{
-    const isMatch = await bcrypt.compare(userPassword,password);
-    return isMatch;
-}
-
-//JS WEBTOKEN
-export function createJWT(id){
-    return JWT.sign({ userId:id }),process.env.JWT_SECRET_KEY,{
-        expiresIn:"id",
-    }
+//JSON WEBTOKEN
+export function createJWT(id) {
+  return JWT.sign({ userId: id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
 }
