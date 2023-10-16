56067705aa8a140ea458a7a31252f7d8652a3842
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-10-16 22:47:23 +0530

    1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import authRoute from './authRoutes.js'
+import userRoute from './userRoutes.js'
+import postRoutes from './postRoutes.js';
 
 const router = express.Router();
 
-router.use('/auth',authRoute);  //auth/register
+router.use(`/auth`,authRoute);  //auth/register
+router.use(`/user`,userRoute);
+router.use(`/posts`, postRoutes);
 
 export default router;
